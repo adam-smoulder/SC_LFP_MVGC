46c6e052b3100e9e782ff9dcc29bfa8ecc23c05f
Adam-Smoulder <asmoulder3195@gmail.com>
2018-05-11 10:22:57 -0400

    Added preprocessing and temp runners for no HP, updated pretag for main mvgcPipeline

---
+++ zztemp/tempRunner051018_2.m
@@ -0,0 +1,341 @@
+
+%% no hp runs mo 25
+load('ND_bb_sc_031315_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+load('ND_bb_sc_031315_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bb_sc_070915_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+load('ND_bb_sc_070915_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bb_sc_071215_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+load('ND_bb_sc_071215_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+
+%
+load('ND_bb_sc_082917_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+load('ND_bb_sc_082917_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+
+%% got to here overnight 5/10/18
+load('ND_bb_sc_121415_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+load('ND_bb_sc_121415_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_blsc_0723151_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+load('ND_blsc_0723151_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_blsc_0723152_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+load('ND_blsc_0723152_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bl_sc_031115_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+load('ND_bl_sc_031115_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bl_sc_071415_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+load('ND_bl_sc_071415_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bl_sc_083017_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+load('ND_bl_sc_083017_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bl_sc_112515_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+load('ND_bl_sc_112515_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 25;
+mvgcTemp4
+close all
+clear
+
+%% mo 62
+
+load('ND_bb_sc_031315_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+load('ND_bb_sc_031315_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bb_sc_070915_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+load('ND_bb_sc_070915_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bb_sc_071215_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+load('ND_bb_sc_071215_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bb_sc_080415_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bb_sc_082917_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+load('ND_bb_sc_082917_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bb_sc_121415_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+load('ND_bb_sc_121415_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_blsc_0723151_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+load('ND_blsc_0723151_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_blsc_0723152_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+load('ND_blsc_0723152_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bl_sc_031115_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+load('ND_bl_sc_031115_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bl_sc_071415_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+load('ND_bl_sc_071415_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bl_sc_083017_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+load('ND_bl_sc_083017_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+%
+load('ND_bl_sc_112515_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'targlfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
+
+load('ND_bl_sc_112515_preproc_det_2_HP_0_Notch_2_1000hz.mat');
+cueString = 'sacclfp';
+modelOrder = 62;
+mvgcTemp4
+close all
+clear
